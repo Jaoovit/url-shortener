eb52956ce6e42033ccd448fa5a67e96d9a6243cc
João Oliveira <jaoovit@gmail.com>
2025-09-22 20:33:20 +0100

    create migration to create url table

---
+++ src/main/resources/db/migration/V1__create-url-table.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE urls (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    long_url VARCHAR(250) NOT NULL,
+    short_url VARCHAR(100) NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    expire_at TIMESTAMP NOT NULL,
+    clicks INTEGER NOT NULL
+)
