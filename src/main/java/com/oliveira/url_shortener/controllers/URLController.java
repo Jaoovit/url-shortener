3528857c385839f2e5935e0192681fa83051357b
João Oliveira <jaoovit@gmail.com>
2025-09-25 20:50:42 +0100

    create controller to send shorten url request

--- src/main/java/com/oliveira/url_shortener/controllers/URLController.java
+++ src/main/java/com/oliveira/url_shortener/controllers/URLController.java
@@ -1,7 +1,12 @@
 package com.oliveira.url_shortener.controllers;
 
+import com.oliveira.url_shortener.domain.URL.ShortUrlDTO;
 import com.oliveira.url_shortener.services.URLService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,11 @@ public class URLController {
 
     @Autowired
     private URLService urlService;
+
+    @PostMapping("/shorten")
+    public ResponseEntity<ShortUrlDTO> shortenUrl(@RequestBody String longUrl, HttpServletRequest request) {
+        ShortUrlDTO shortUrl = urlService.shortenUrl(longUrl, request);
+        return ResponseEntity.ok(shortUrl);
+    }
 
 }
