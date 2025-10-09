3b9c98312c1fcd90406e7a5ac360e43e091c1b07
João Oliveira <jaoovit@gmail.com>
2025-10-09 19:14:31 +0100

    create controller to redirect

--- src/main/java/com/oliveira/url_shortener/controllers/URLController.java
+++ src/main/java/com/oliveira/url_shortener/controllers/URLController.java
@@ -5,20 +5,33 @@ import com.oliveira.url_shortener.domain.URL.ShortUrlDTO;
 import com.oliveira.url_shortener.services.URLService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/url")
 public class URLController {
 
     @Autowired
     private URLService urlService;
 
-    @PostMapping("/shorten")
+    @PostMapping("/api/url/shorten")
     public ResponseEntity<ShortUrlDTO> shortenUrl(@RequestBody LongUrlDTO longUrlDTO, HttpServletRequest request) {
         ShortUrlDTO shortUrl = urlService.shortenUrl(longUrlDTO.longUrl(), request);
         return ResponseEntity.ok(shortUrl);
+    }
+
+    @GetMapping("/{hash}")
+    public ResponseEntity<Void> redirect(@PathVariable String hash,  HttpServletRequest request) {
+        try {
+            LongUrlDTO longUrl = urlService.redirect(hash);
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .header("Location", longUrl)
+                    .build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
     }
 
 }
